c4c7e93e2fb05d12333c1236592aa043cb845b43
Salgac <domco.salgovic@gmail.com>
2024-04-29 15:10:28 +0200

    Added custom metrics

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -1,50 +1,102 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "github.com/gin-gonic/gin"
-    "github.com/Salgac/wac-ambulance-webapi/api"  
-    "github.com/Salgac/wac-ambulance-webapi/internal/ambulance_wl"
-    "github.com/Salgac/wac-ambulance-webapi/internal/db_service"
-    "context"
-    "time"
-    "github.com/gin-contrib/cors"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Salgac/wac-ambulance-webapi/api"
+	"github.com/Salgac/wac-ambulance-webapi/internal/ambulance_wl"
+	"github.com/Salgac/wac-ambulance-webapi/internal/db_service"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/technologize/otel-go-contrib/otelginmetrics"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    corsMiddleware := cors.New(cors.Config{
-        AllowOrigins:     []string{"*"},
-        AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
-        AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-        ExposeHeaders:    []string{""},
-        AllowCredentials: false,
-        MaxAge: 12 * time.Hour,
-    })
-    engine.Use(corsMiddleware)
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
 
-    // setup context update  middleware
-    dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
-    defer dbService.Disconnect(context.Background())
-    engine.Use(func(ctx *gin.Context) {
-        ctx.Set("db_service", dbService)
-        ctx.Next()
-    })
-    
-    // request routings
-    ambulance_wl.AddRoutes(engine)
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	// setup telemetry
+	initTelemetry()
+
+	engine.Use(otelginmetrics.Middleware(
+		"Ambulance WebAPI Service",
+		// Custom attributes
+		otelginmetrics.WithAttributes(func(serverName, route string, request *http.Request) []attribute.KeyValue {
+			return append(otelginmetrics.DefaultAttributes(serverName, route, request))
+		}),
+	))
+
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+
+	// metrics endpoint
+	promhandler := promhttp.Handler()
+	engine.Any("/metrics", func(ctx *gin.Context) {
+		promhandler.ServeHTTP(ctx.Writer, ctx.Request)
+	})
+
+	// request routings
+	ambulance_wl.AddRoutes(engine)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
+}
+
+// initialize OpenTelemetry instrumentations
+func initTelemetry() error {
+	ctx := context.Background()
+	res, err := resource.New(ctx,
+		resource.WithAttributes(semconv.ServiceNameKey.String("Ambulance WebAPI Service")),
+		resource.WithAttributes(semconv.ServiceNamespaceKey.String("WAC Hospital")),
+		resource.WithSchemaURL(semconv.SchemaURL),
+		resource.WithContainer(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	metricExporter, err := prometheus.New()
+	if err != nil {
+		return err
+	}
+
+	metricProvider := metric.NewMeterProvider(metric.WithReader(metricExporter), metric.WithResource(res))
+	otel.SetMeterProvider(metricProvider)
+	return nil
 }
--- internal/ambulance_wl/utils_ambulance_updater.go
+++ internal/ambulance_wl/utils_ambulance_updater.go
@@ -1,113 +1,183 @@
 package ambulance_wl
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Salgac/wac-ambulance-webapi/internal/db_service"
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 )
 
+var (
+	dbMeter           = otel.Meter("waiting_list_access")
+	dbTimeSpent       metric.Float64Counter
+	waitingListLength = map[string]int64{}
+)
+
+// package initialization - called automaticaly by go runtime when package is used
+func init() {
+	// initialize OpenTelemetry instrumentations
+	var err error
+	dbTimeSpent, err = dbMeter.Float64Counter(
+		"ambulance_wl_time_spent_in_db",
+		metric.WithDescription("The time spent in the database requests"),
+		metric.WithUnit("ms"),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
 type ambulanceUpdater = func(
-    ctx *gin.Context,
-    ambulance *Ambulance,
+	ctx *gin.Context,
+	ambulance *Ambulance,
 ) (updatedAmbulance *Ambulance, responseContent interface{}, status int)
 
 func updateAmbulanceFunc(ctx *gin.Context, updater ambulanceUpdater) {
-    value, exists := ctx.Get("db_service")
-    if !exists {
-        ctx.JSON(
-            http.StatusInternalServerError,
-            gin.H{
-                "status":  "Internal Server Error",
-                "message": "db_service not found",
-                "error":   "db_service not found",
-            })
-        return
-    }
+	value, exists := ctx.Get("db_service")
+	if !exists {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "db_service not found",
+				"error":   "db_service not found",
+			})
+		return
+	}
 
-    db, ok := value.(db_service.DbService[Ambulance])
-    if !ok {
-        ctx.JSON(
-            http.StatusInternalServerError,
-            gin.H{
-                "status":  "Internal Server Error",
-                "message": "db_service context is not of type db_service.DbService",
-                "error":   "cannot cast db_service context to db_service.DbService",
-            })
-        return
-    }
+	db, ok := value.(db_service.DbService[Ambulance])
+	if !ok {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "db_service context is not of type db_service.DbService",
+				"error":   "cannot cast db_service context to db_service.DbService",
+			})
+		return
+	}
 
-    ambulanceId := ctx.Param("ambulanceId")
+	ambulanceId := ctx.Param("ambulanceId")
 
-    ambulance, err := db.FindDocument(ctx, ambulanceId)
+	start := time.Now()
+	ambulance, err := db.FindDocument(ctx, ambulanceId)
+	dbTimeSpent.Add(ctx, float64(float64(time.Since(start)))/float64(time.Millisecond), metric.WithAttributes(
+		attribute.String("operation", "find"),
+		attribute.String("ambulance_id", ambulanceId),
+		attribute.String("ambulance_name", ambulance.Name),
+	))
 
-    switch err {
-    case nil:
-        // continue
-    case db_service.ErrNotFound:
-        ctx.JSON(
-            http.StatusNotFound,
-            gin.H{
-                "status":  "Not Found",
-                "message": "Ambulance not found",
-                "error":   err.Error(),
-            },
-        )
-        return
-    default:
-        ctx.JSON(
-            http.StatusBadGateway,
-            gin.H{
-                "status":  "Bad Gateway",
-                "message": "Failed to load ambulance from database",
-                "error":   err.Error(),
-            })
-        return
-    }
+	switch err {
+	case nil:
+		// continue
+	case db_service.ErrNotFound:
+		ctx.JSON(
+			http.StatusNotFound,
+			gin.H{
+				"status":  "Not Found",
+				"message": "Ambulance not found",
+				"error":   err.Error(),
+			},
+		)
+		return
+	default:
+		ctx.JSON(
+			http.StatusBadGateway,
+			gin.H{
+				"status":  "Bad Gateway",
+				"message": "Failed to load ambulance from database",
+				"error":   err.Error(),
+			})
+		return
+	}
 
-    if !ok {
-        ctx.JSON(
-            http.StatusInternalServerError,
-            gin.H{
-                "status":  "Internal Server Error",
-                "message": "Failed to cast ambulance from database",
-                "error":   "Failed to cast ambulance from database",
-            })
-        return
-    }
+	if !ok {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "Failed to cast ambulance from database",
+				"error":   "Failed to cast ambulance from database",
+			})
+		return
+	}
 
-    updatedAmbulance, responseObject, status := updater(ctx, ambulance)
+	updatedAmbulance, responseObject, status := updater(ctx, ambulance)
 
-    if updatedAmbulance != nil {
-        err = db.UpdateDocument(ctx, ambulanceId, updatedAmbulance)
-    } else {
-        err = nil // redundant but for clarity
-    }
+	if updatedAmbulance != nil {
+		start := time.Now()
+		err = db.UpdateDocument(ctx, ambulanceId, updatedAmbulance)
+		// update metrics
+		dbTimeSpent.Add(ctx, float64(float64(time.Since(start)))/float64(time.Millisecond), metric.WithAttributes(
+			attribute.String("operation", "update"),
+			attribute.String("ambulance_id", ambulanceId),
+			attribute.String("ambulance_name", ambulance.Name),
+		))
 
-    switch err {
-    case nil:
-        if responseObject != nil {
-            ctx.JSON(status, responseObject)
-        } else {
-            ctx.AbortWithStatus(status)
-        }
-    case db_service.ErrNotFound:
-        ctx.JSON(
-            http.StatusNotFound,
-            gin.H{
-                "status":  "Not Found",
-                "message": "Ambulance was deleted while processing the request",
-                "error":   err.Error(),
-            },
-        )
-    default:
-        ctx.JSON(
-            http.StatusBadGateway,
-            gin.H{
-                "status":  "Bad Gateway",
-                "message": "Failed to update ambulance in database",
-                "error":   err.Error(),
-            })
-    }
+		// demonstration of possible handling of async instruments:
+		// not really an operational metric, it would be more of a business metric/KPI.
+		// also UpDownCounter may be of better use in practical cases.
+		if _, ok := waitingListLength[ambulanceId]; !ok {
+			newGauge, err := dbMeter.Int64ObservableGauge(
+				fmt.Sprintf("%v_waiting_patients", ambulanceId),
+				metric.WithDescription(fmt.Sprintf("The length of the waiting list for the ambulance %v", ambulance.Name)),
+				metric.WithUnit("{patient}"),
+			)
+			if err != nil {
+				log.Printf("Failed to create waiting list length gauge for ambulance %v: %v", ambulanceId, err)
+			}
+			waitingListLength[ambulanceId] = 0
+
+			_, err = dbMeter.RegisterCallback(func(_ context.Context, o metric.Observer) error {
+				// we could have looked up the ambulance in the database here, but we already have it in memory
+				// so use the latest snapshots to update the gauge
+				o.ObserveInt64(newGauge, waitingListLength[ambulanceId])
+				return nil
+			}, newGauge)
+
+			if err != nil {
+				log.Printf("Failed to register callback for waiting list length gauge for ambulance %v: %v", ambulanceId, err)
+			}
+		}
+
+		// set the gauge snapshot
+		waitingListLength[ambulanceId] = int64(len(updatedAmbulance.WaitingList))
+	} else {
+		err = nil // redundant but for clarity
+	}
+
+	switch err {
+	case nil:
+		if responseObject != nil {
+			ctx.JSON(status, responseObject)
+		} else {
+			ctx.AbortWithStatus(status)
+		}
+	case db_service.ErrNotFound:
+		ctx.JSON(
+			http.StatusNotFound,
+			gin.H{
+				"status":  "Not Found",
+				"message": "Ambulance was deleted while processing the request",
+				"error":   err.Error(),
+			},
+		)
+	default:
+		ctx.JSON(
+			http.StatusBadGateway,
+			gin.H{
+				"status":  "Bad Gateway",
+				"message": "Failed to update ambulance in database",
+				"error":   err.Error(),
+			})
+	}
 
 }
